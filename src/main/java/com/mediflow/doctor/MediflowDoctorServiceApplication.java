f8eeaf1b46181e5a1d96afef9b2be9ddf749add9
jerome784 <jerinjerome22@gmail.com>
2026-08-01 17:47:32 +0530

    feat: add doctor service bootstrap

---
+++ src/main/java/com/mediflow/doctor/MediflowDoctorServiceApplication.java
@@ -0,0 +1,15 @@
+package com.mediflow.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MediflowDoctorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediflowDoctorServiceApplication.class, args);
+    }
+
+}
