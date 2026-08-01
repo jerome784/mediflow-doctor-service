745c9889e506c9a647f1a83796c97063e83c0a44
jerome784 <jerinjerome22@gmail.com>
2026-08-01 17:47:34 +0530

    test: add application smoke test

---
+++ src/test/java/com/mediflow/doctor/MediflowDoctorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediflow.doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediflowDoctorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
